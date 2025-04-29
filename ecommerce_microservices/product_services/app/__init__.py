4d799b5831cacd55645f64a9e714ac399cc8a80c
AmanSg098 <singhaman123890@gmail.com>
2025-04-29 13:09:54 +0530

    Structured Order Services

--- ecommerce_microservices/product_services/app/__init__.py
+++ ecommerce_microservices/product_services/app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from flask_migrate import Migrate 
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+jwt = JWTManager()
 
 def create_app(config=Config):
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app(config=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     from .routes.products import product_bp
     app.register_blueprint(product_bp, url_prefix='/api/products')
--- ecommerce_microservices/product_services/app/jwt_utils.py
+++ ecommerce_microservices/product_services/app/jwt_utils.py
@@ -1,26 +1,13 @@
-import jwt
-from flask import request, jsonify, current_app
+from flask_jwt_extended import verify_jwt_in_request
 from functools import wraps
+from flask import jsonify
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'Authorization' in request.headers:
-            token = request.headers['Authorization'].split(" ")[1]
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-
+def token_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
         try:
-            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
-            # Optional: Add user info to request context if needed
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token has expired!'}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Invalid token!'}), 401
-
-        return f(*args, **kwargs)
-
-    return decorated
+            verify_jwt_in_request()
+        except Exception as e:
+            return jsonify({"message": str(e)}), 401
+        return fn(*args, **kwargs)
+    return wrapper
