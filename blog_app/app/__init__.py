ce189d03199d4abe8a918138d0b896bd63c505f4
AmanSg098 <singhaman123890@gmail.com>
2025-05-05 22:12:27 +0530

    Added Get Post by Id and Update Post routes

--- blog_app/app/__init__.py
+++ blog_app/app/__init__.py
@@ -18,8 +18,10 @@ def create_app(config=Config):
 
     from .auth.auth import auth_bp
     from .user.user import user_bp
+    from .post.posts import posts_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
+    app.register_blueprint(posts_bp)
 
     return app
--- blog_app/app/post/posts.py
+++ blog_app/app/post/posts.py
@@ -4,9 +4,9 @@ from app import db
 from app.models import Post, Tag, User
 from slugify import slugify
 
-posts_bp = Blueprint('posts', __name__, url_prefix='/api/posts')
+posts_bp = Blueprint('posts', __name__, url_prefix='/posts')
 
-@posts_bp.route('/', methods=['POST'])
+@posts_bp.route('/create', methods=['POST'])
 @jwt_required()
 def create_post():
     data = request.get_json()
@@ -44,3 +44,58 @@ def create_post():
     db.session.commit()
 
     return jsonify({"message": "Post created", "post_id": post.id}), 201
+
+
+@posts_bp.route('/<int:post_id>', methods=['GET'])
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.views += 1
+    db.session.commit()
+
+    return jsonify(
+        {
+            "id": post.id,
+            "title": post.title,
+            "slug": post.slug,
+            "content": post.content,
+            "author": post.author.username,
+            "tags": [tag.name for tag in post.tags],
+            "views": post.views,
+            "likes": len(post.likes),
+            "comments": len(post.comments),
+            "created_at": post.created_at,
+            "updated_at": post.updated_at
+        }
+    ), 200
+
+
+@posts_bp.route('/<int:post_id>', methods=['PUT'])
+@jwt_required()
+def update_post(post_id):
+    user_mail = get_jwt_identity()
+    user = User.query.filter_by(email=user_mail).first()
+    post = Post.query.get_or_404(post_id)
+
+    if post.user_id != user.id:
+        return jsonify({'error': 'Unauthorized'}), 403
+
+    data = request.get_json()
+    post.title = data.get('title', post.title)
+    post.slug = slugify(post.title)
+    post.content = data.get('content', post.content)
+    post.status = data.get('status', post.status)
+    post.image_url = data.get('image_url', post.image_url)
+
+    # Handle tags
+    tags = data.get('tags')  # Expecting list of tag names
+    if tags is not None:
+        post.tags.clear()
+        for tag_name in tags:
+            tag = Tag.query.filter_by(name=tag_name).first()
+            if not tag:
+                tag = Tag(name=tag_name)
+                db.session.add(tag)
+            post.tags.append(tag)
+
+    db.session.commit()
+    return jsonify({'message': 'Post updated'}), 200
